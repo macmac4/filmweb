2609f3f3d2d729a714a074343ef6d2d9814e566f
Maciej B <maciej@galactica.pl>
2021-01-24 23:03:04 +0100

    add singup & login

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import LoginForm from "../components/LoginForm.vue";
+import SignUpForm from "../components/SignUpForm.vue";
 
 const routes = [
   {
@@ -10,11 +13,17 @@ const routes = [
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: About
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: LoginForm
+  },
+  {
+    path: "/signup",
+    name: "SignUp",
+    component: SignUpForm
   }
 ];
 
